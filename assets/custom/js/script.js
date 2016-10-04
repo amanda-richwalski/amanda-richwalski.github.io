5d1a59c13ad7f405212e1c6b0a9d8068d102a401
Amanda Richwalski <amandarichwalski@admins-MacBook-Pro.local>
2016-10-04 16:47:19 -0400

    more portfolio

--- assets/custom/js/script.js
+++ assets/custom/js/script.js
@@ -361,7 +361,7 @@
 
                 // Create a map object, and include the MapTypeId to add
                 // to the map type control.
-                var $latlng = new google.maps.LatLng(29.198351, -81.028046),
+                var $latlng = new google.maps.LatLng(39.9632427,-86.2724681),
                     $mapOptions = {
                         zoom: 13,
                         center: $latlng,
